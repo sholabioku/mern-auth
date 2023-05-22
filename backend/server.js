08f10b0f6743da47134675f12d7a05b22ab8743c
sholabioku <bilushonline@gmail.com>
2023-05-22 12:09:57 +0100

    Add functionality for custom error handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 
 import userRoutes from './routes/userRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -10,5 +11,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`.yellow));
