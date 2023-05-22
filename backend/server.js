cfc1fa1bd09749123cd280e57f83266cdf6015a8
sholabioku <bilushonline@gmail.com>
2023-05-22 11:43:06 +0100

    Add routes and controller setup for users

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/userRoutes.js';
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Server is ready'));
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`.yellow));
