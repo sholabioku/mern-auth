2e9e56ba8ec36e2eaca51ce96b48e95fdabaa003
sholabioku <bilushonline@gmail.com>
2023-06-02 07:56:35 +0100

    Add functionality to generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc Auth user/set token
 // @route POST /api/users/auth
@@ -23,6 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const user = await User.create({ name, email, password });
+  generateToken(res, user._id);
   if (user) {
     res.status(201).json({
       _id: user._id,
