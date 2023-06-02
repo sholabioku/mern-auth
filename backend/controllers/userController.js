2d6b16336564f1dcb780b99be56542f863cdf9b1
sholabioku <bilushonline@gmail.com>
2023-06-02 08:11:18 +0100

    Add functionality to authenticate user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,6 +7,20 @@ import generateToken from '../utils/generateToken.js';
 // @route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+
   res.status(200).json({ message: 'Auth User' });
 });
 
