c75ced58560695358c58162bf9908c24641025ac
sholabioku <bilushonline@gmail.com>
2023-06-06 09:33:27 +0100

    Add setup for route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+
 import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
-      <HomeScreen />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   );
 };
