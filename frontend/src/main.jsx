476a7932c03a5276ce560dc4fc73e22e38bae397
sholabioku <bilushonline@gmail.com>
2023-06-08 14:17:48 +0100

    Add functionality to setup redux store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,6 +15,7 @@ import './index.css';
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
+import store from './store.js';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,7 +28,9 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
